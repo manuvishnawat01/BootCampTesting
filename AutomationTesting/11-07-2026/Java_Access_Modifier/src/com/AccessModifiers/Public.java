2e4c15d1ef2c76eb429e68b76146da28e1fd2030
Manu Pratap Singh <manu.pratap2004@gmail.com>
2026-07-17 02:24:06 +0530

    Fix Public class implementation and method calls

--- AutomationTesting/11-07-2026/Java_Access_Modifier/src/com/AccessModifiers/Public.java
+++ AutomationTesting/11-07-2026/Java_Access_Modifier/src/com/AccessModifiers/Public.java
@@ -1,12 +1,23 @@
 package com.AccessModifiers;
 
-public class Public extends B{
-	
-	public static void main(String[] args) {
-	Public obj = new public();
-	int output = new public();
-	System.out.println("value of x is : "+ output);
-	obj.b1();
-	}
+class ParentClassB {
+    public int x() {
+        return 10;
+    }
+
+    public void b1() {
+        System.out.println("Public b1 method executed successfully.");
+    }
 }
 
+public class Public extends ParentClassB {
+    
+    public static void main(String[] args) {
+        Public obj = new Public();
+        
+        int output = obj.x();
+        System.out.println("value of x is : " + output);
+        
+        obj.b1();
+    }
+}
